83f95676e989b9bd03eb5af9119bb0629c7826ba
Atsushi Mori <ki4070ma@gmail.com>
2019-01-01 22:54:49 +0900

    Added No.4

---
+++ 0004_Median_of_Two_Sorted_Arrays.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+class Solution(object):
+    def findMedianSortedArrays(self, nums1, nums2):
+        """
+        :type nums1: List[int]
+        :type nums2: List[int]
+        :rtype: float
+        """
+        l = sorted(nums1 + nums2)
+        length = len(l)
+        if len(l) % 2 == 1:
+            return l[length/2]
+        else:
+            return (l[length/2 - 1] + l[length/2])/2.0
