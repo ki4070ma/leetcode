8c93059b20438208c0606b7f70a01399299dbc52
AtsushiB.Mori <Atsushi.B.Mori@sony.com>
2019-01-16 18:39:20 +0900

    Added No.35

---
+++ 0035_Search_Insert_Position.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+class Solution(object):
+    def searchInsert(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+        for i, x in enumerate(nums):
+            if x >= target:
+                return i
+        return len(nums)
