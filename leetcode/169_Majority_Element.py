bf91f6b12de8f87b6700bb228b5b40333a13af42
Atsushi Mori <ki4070ma@gmail.com>
2019-08-01 06:47:55 +0900

    [Update][Leetcode] 88 Merge Sorted Array

---
+++ leetcode/169_Majority_Element.py
@@ -0,0 +1,9 @@
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+        d = {}
+        for n in nums:
+            d[n] = d[n]+1 if n in d.keys() else 1
+        val, ret = 0, 0
+        for key in d.keys():
+            if val < d[key]: ret, val = key, d[key]
+        return ret
