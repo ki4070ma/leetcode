16e2ef304355fe6f1baecb7ce6477e69c999fee3
Atsushi Mori <ki4070ma@gmail.com>
2019-08-17 13:33:00 +0900

    [LeetCode] 938. Range Sum of BST(2)

--- leetcode/0938_Range_Sum_of_BST.py
+++ leetcode/0938_Range_Sum_of_BST.py
@@ -22,3 +22,19 @@ class Solution:
         self.target_list.append(node.val)
         if node.right:
             self.showNodeVal(node.right)
+
+# From discuss
+class Solution:
+
+    def rangeSumBST(self, root: TreeNode, L: int, R: int) -> int:
+        target_list = []
+        return sum(self.store_target_list(root, L, R, target_list))
+
+    def store_target_list(self, node, L, R, target_list):
+        if node.left:
+            self.store_target_list(node.left, L, R, target_list)
+        if node.val >= L and node.val <= R:
+            target_list.append(node.val)
+        if node.right:
+            self.store_target_list(node.right, L, R, target_list)
+        return target_list
