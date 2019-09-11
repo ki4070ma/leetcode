def871bbb584a167d7a1cd956f5ac57fe92a3db4
Atsushi Mori <ki4070ma@gmail.com>
2019-09-11 22:06:56 +0900

    [AtCoder] B Power Socket

---
+++ atcoder/20190901_ABC/B_PowerSocket.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+num_power_taps, target_tap_num = map(int, input().split())
+
+if target_tap_num == 1:
+  print(0)
+else:
+  print(-(-(target_tap_num - 1) // (num_power_taps - 1)))
