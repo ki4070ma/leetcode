7255f64e5c82f8e9d83f156652722533b501c854
Atsushi Mori <ki4070ma@gmail.com>
2019-09-15 22:48:22 +0900

    [AtCoder] C AttackSurvival

---
+++ atcoder/20190915_ABC/C_AttackSurvival.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+from sys import stdin
+
+for _ in range(3):
+    # print('***')
+    N, K, Q = map(int, stdin.readline().rstrip().split())
+    results = {}
+    for i in range(N):
+        results[str(i + 1)] = 0
+    # print(results)
+    for i in range(Q):
+        S = stdin.readline().rstrip()
+        results[S] += 1
+    for i in range(N):
+        if K - (Q - results[str(i + 1)]) > 0:
+            print('Yes')
+        else:
+            print('No')
