a51942458777ac9b52e037e1e2998ff9aa1c1493
Atsushi Mori <ki4070ma@gmail.com>
2019-09-30 00:33:02 +0900

    [AtCoder][WIP] D Disjoint Set of Common Divisors

---
+++ atcoder/20190928_ABC/D_DisjointSetOfCommonDivisors.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+
+from sys import stdin
+
+for _ in range(2):
+    A, B = map(int, stdin.readline().rstrip().split())
+
+    As = []
+    Bs = []
+
+    ret = []
+    for i in range(2, min(A, B)+1):
+        if A % i == 0 and B % i == 0:
+            ret.append(i)
+
+    print(ret[::-1])
+
+    result = []
+    for i, a in enumerate(ret):
+        data = []
+        for b in ret[i+1:]:
+            tmp = 'T' if b % a == 0 else 'F'
+            data.append(tmp)
+        if data:
+            print([a] + data[::-1])
