de7fd4b37827bd100b3ef4de3bfecffdd35c24b9
Atsushi Mori <ki4070ma@gmail.com>
2019-11-27 23:42:36 +0900

    [AtCoder] ABC146 B

---
+++ atcoder/20191124_ABC/B_ROTN.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+
+from sys import stdin
+
+for _ in range(3):
+    N = int(stdin.readline().rstrip())
+    S = stdin.readline().rstrip()
+    new_str = ''
+    for a in list(S):
+        ascii_code = ord(a) + N
+        ascii_code = (ascii_code - 26) if ascii_code > 90 else ascii_code
+        new_str += chr(ascii_code)
+    print(new_str)
+
+# Answer
+# N = int(input().rstrip())
+# S = input().rstrip()
+# new_str = ''
+# for a in list(S):
+#     ascii_code = ord(a) + N
+#     ascii_code = (ascii_code - 26) if ascii_code > 90 else ascii_code
+#     new_str += chr(ascii_code)
+# print(new_str)
