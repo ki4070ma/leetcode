7e1e48907a195397becf99ebdb567961859b83ce
Atsushi Mori <ki4070ma@gmail.com>
2019-12-23 00:36:39 +0900

    [AtCoder] ABC148 C

---
+++ atcoder/20191222_ABC/C_Snack.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+
+from sys import stdin
+
+for _ in range(3):
+    A, B = map(int, stdin.readline().rstrip().split())
+
+    X = A * B
+
+    # greatest_common_divisor
+    if A < B:
+        tmp = B
+        B = A
+        A = tmp
+
+    r = A % B
+    while (r != 0):
+        A = B
+        B = r
+        r = A % B
+
+    print(int(X / B))
