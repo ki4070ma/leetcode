a9193449a7ea2c0e1f88308f18b7612d61cc964c
Atsushi Mori <ki4070ma@gmail.com>
2019-12-29 21:48:00 +0900

    [AtCoder] ABC149 C

---
+++ atcoder/20191229_ABC/C_Next_Prime.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+
+from sys import stdin
+
+def is_prime(num):
+    if num < 2:
+        return False
+    elif num == 2:
+        return True
+    elif num % 2 == 0:
+        return False
+
+    from math import sqrt
+    sqrtnum = sqrt(num)
+    i = 3
+    while (i <= sqrtnum):
+        if num % i == 0:
+            return False
+        i += 2
+    return True
+
+for _ in range(3):
+    X = int(stdin.readline().rstrip())
+
+    while True:
+        if is_prime(X):
+            print(X)
+            break
+        X += 1
