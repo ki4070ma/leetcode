305954de9c595f564db936626c07834e23155a61
Atsushi Mori <ki4070ma@gmail.com>
2019-12-30 23:51:13 +0900

    chore: [AtCoder] ABC149 C

--- atcoder/20191229_ABC/C_Next_Prime.py
+++ atcoder/20191229_ABC/C_Next_Prime.py
@@ -2,6 +2,16 @@
 
 from sys import stdin
 
+def is_prime_editorial(num):
+    if num <= 1:
+        return False
+    i = 2
+    while (i*i < num):
+        if num % i == 0:
+            return False
+        i += 1
+    return True
+
 def is_prime(num):
     if num < 2:
         return False
