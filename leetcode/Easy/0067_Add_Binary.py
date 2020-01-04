6f9692d6eda5ad43d21bd69a50578a5909476219
Atsushi Mori <ki4070ma@gmail.com>
2020-01-04 13:27:02 +0900

    [LeetCode] Easy 0067

---
+++ leetcode/Easy/0067_Add_Binary.py
@@ -0,0 +1,27 @@
+class Solution:
+    def addBinary(self, a: str, b: str) -> str:
+        ret = ''
+        length = max(len(a), len(b))
+        kuriage = False
+        a = a[::-1]
+        b = b[::-1]
+        for i in range(length):
+            ret_val = 1 if kuriage else 0
+            kuriage = False
+            if len(a) > i and len(b) > i:
+                #                print('a: {}, b: {}'.format(a[i], b[i]))
+                ret_val += int(a[i]) + int(b[i])
+            elif len(a) > i:
+                #                print('a: {}'.format(a[i]))
+                ret_val += int(a[i])
+            elif len(b) > i:
+                #                print('b: {}'.format(b[i]))
+                ret_val += int(b[i])
+            if ret_val > 1:
+                kuriage = True
+                ret_val = ret_val % 2
+            ret = str(ret_val) + ret
+        else:
+            if kuriage:
+                ret = '1' + ret
+        return ret
