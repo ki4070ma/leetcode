173df4e6c669ff5d0c800d72ffad2c006537a4ef
Atsushi Mori <ki4070ma@gmail.com>
2020-01-05 21:23:01 +0900

    [LeetCode] Midium 0034 (ANSWER)

---
+++ leetcode/Midium/0034_ANSWER_Find_First_and_Last_Position_of_Element_in_Sorted_Array.py
@@ -0,0 +1,20 @@
+class Solution:
+    def searchRange(self, nums: List[int], target: int) -> List[int]:
+        left_idx = self.extreme_insertion_index(nums, target, True)
+
+        if left_idx == len(nums) or nums[left_idx] != target:
+            return [-1, -1]
+
+        return [left_idx, self.extreme_insertion_index(nums, target, False) - 1]
+
+    def extreme_insertion_index(self, nums, target, left):
+        lo = 0
+        hi = len(nums)
+
+        while lo < hi:
+            mid = (lo + hi) // 2
+            if nums[mid] > target or (left and nums[mid] == target):
+                hi = mid
+            else:
+                lo = mid + 1
+        return lo
