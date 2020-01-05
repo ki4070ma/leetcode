cde35d89b6a9f18189a46e740a7f5680d2c71d7c
Atsushi Mori <ki4070ma@gmail.com>
2020-01-05 12:15:18 +0900

    [LeetCode] Easy 0122

---
+++ leetcode/Easy/0122_Answer_Best_Time_to_buy_and_Sell_Stock_2.py
@@ -0,0 +1,7 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        ret = 0
+        for i in range(1, len(prices)):
+            if prices[i-1] < prices[i]:
+                ret += prices[i] - prices[i-1]
+        return ret
