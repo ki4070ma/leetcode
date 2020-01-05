6926353362d04029d4253e8fa003e5a5e07b8f6f
Atsushi Mori <ki4070ma@gmail.com>
2020-01-05 11:57:48 +0900

    [LeetCode] Easy 0118

---
+++ leetcode/Easy/0118_Pascals_Triangle.py
@@ -0,0 +1,19 @@
+class Solution:
+    def generate(self, numRows: int) -> List[List[int]]:
+        ret = [[]]
+        if numRows == 0:
+            ret = []
+        elif numRows == 1:
+            ret = [[1]]
+        else:
+            ret = [[1], [1,1]]
+        if numRows > 2:
+            for i in range(1, numRows-1):
+                tmp = [1]
+                for j, _ in enumerate(ret[i]):
+                    if j+1 < len(ret[i]):
+                        tmp.append(ret[i][j]+ret[i][j+1])
+                else:
+                    tmp.append(1)
+                ret.append(tmp)
+        return ret
