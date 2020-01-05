788cf35b975017794f3a443a947f6dbef274d4ae
Atsushi Mori <ki4070ma@gmail.com>
2020-01-05 11:57:25 +0900

    [LeetCode] Easy 0083

---
+++ leetcode/Easy/0083_Remove_Duplicates_from_Sorted_List.py
@@ -0,0 +1,22 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def deleteDuplicates(self, head: ListNode) -> ListNode:
+        if head is None:
+            return head
+
+        current = head.next
+        prev = head
+
+        while current is not None:
+            if current.val == prev.val:
+                prev.next = current.next
+                current = current.next
+            else:
+                current = current.next
+                prev = prev.next
+        return head
