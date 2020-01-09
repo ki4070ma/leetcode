dfc28b8b07252e87b74ccedb1e407ce773ea35b5
Atsushi Mori <ki4070ma@gmail.com>
2020-01-10 01:17:39 +0900

    [LeetCode] Easy 0217

--- leetcode/Easy/0136_ANSWER_Single_Number.py
+++ leetcode/Easy/0136_ANSWER_Single_Number.py
@@ -1,3 +1,8 @@
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
+        ret = 0
+        for num in nums:
+            ret ^= num
+        return ret
+
         return 2 * sum(list(set(nums))) - sum(nums)
---
+++ leetcode/Easy/217_Contains_Duplicate.py
@@ -0,0 +1,13 @@
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        if len(nums) == 0:
+            return False
+        d = {}
+        for num in nums:
+            if d.get(num, 'a') == 'a':
+                d[num] = 1
+            else:
+                d[num] += 1
+        if sum(d.values()) / len(d.values()) > 1:
+            return True
+        return False
