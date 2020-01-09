2dccdad6379b78e6dc3cf420069f929878e243c4
Atsushi Mori <ki4070ma@gmail.com>
2020-01-10 01:53:49 +0900

    [LeetCode] Easy 0219 (TLE)
    So bad codes

---
+++ leetcode/Easy/0219_TLE_Contains_Duplicate_II.py
@@ -0,0 +1,28 @@
+class Solution:
+    def containsNearbyDuplicate(self, nums: List[int], k: int) -> bool:
+        if len(nums) < k:
+            k = len(nums)
+
+        if len(nums) == k and (k == 0 or k == 1):
+            return False
+        elif len(nums) == k:
+            d = {}
+            for n in nums:
+                if d.get(n, 'a') == 'a':
+                    d[n] = 1
+                else:
+                    d[n] += 1
+            if sum(d.values()) / len(d.values()) > 1:
+                return True
+
+        for i in range(len(nums) - k):
+            d = {}
+            for n in nums[i:i + k + 1]:
+                if d.get(n, 'a') == 'a':
+                    d[n] = 1
+                else:
+                    d[n] += 1
+            #            print(d)
+            if sum(d.values()) / len(d.values()) > 1:
+                return True
+        return False
