5d35161d5747c1e4eb67491f2b7c5a5671cc0f10
Atsushi Mori <ki4070ma@gmail.com>
2020-01-11 12:31:40 +0900

    [AtCoder] ABC150 B

---
+++ atcoder/20200110_ABC/B_CountABC.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+from sys import stdin
+
+for _ in range(7):
+    abc = 'ABC'
+    N = int(stdin.readline().rstrip())
+    S = stdin.readline().rstrip()
+#    print('N: {}, S: {}'.format(N, S))
+    ret = 0
+    if N == 3 and S == abc:
+        ret = 1
+    else:
+        for i in range(N - len(abc) + 1):
+            if abc == S[i:i + 3]:
+                ret += 1
+    print(ret)
