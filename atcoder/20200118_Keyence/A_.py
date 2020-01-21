5b1e1dde577accc853ba825fb1b1ed62eb951075
Atsushi Mori <ki4070ma@gmail.com>
2020-01-21 23:56:05 +0900

    [AtCoder] Keyence A

---
+++ atcoder/20200118_Keyence/A_.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+for _ in range(3):
+    H = int(input().rstrip())
+    W = int(input().rstrip())
+    N = int(input().rstrip())
+    longer = max(H, W)
+    if N % longer == 0:
+        print(N // longer)
+    else:
+        print(N // longer + 1)
+
+
