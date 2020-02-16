cc76e27846516e6b599036e0fb791fe732087d4c
Atsushi Mori <ki4070ma@gmail.com>
2020-02-16 23:52:10 +0900

    [AtCoder] ABC 155 (A)

---
+++ atcoder/20200216_ABC/A_.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+for _ in range(4):
+    l = [int(x) for x in input().rstrip().split()]
+    d = {}
+    for x in l:
+        if x in d.keys():
+            d[x] += 1
+        else:
+            d[x] = 1
+    if 2 in d.values():
+        print('Yes')
+    else:
+        print('No')
