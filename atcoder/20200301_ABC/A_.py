ca6cd46985a2a82d4ef077baa2249f150097786e
Atsushi Mori <ki4070ma@gmail.com>
2020-03-02 23:42:43 +0900

    [AtCoder] ABC 157 (A-2)

--- atcoder/20200301_ABC/A_.py
+++ atcoder/20200301_ABC/A_.py
@@ -2,7 +2,16 @@
 
 for _ in range(3):
     N = int(input().rstrip())
-    if N % 2 == 0:
-        print(int(N / 2))
-    else:
-        print(int(N // 2 + 1))
+
+    # Answer1
+    # print(N//2 + N % 2)
+
+    # Answer2
+    import math
+    print(math.ceil(N/2))
+
+    # Answer3
+    # if N % 2 == 0:
+    #     print(int(N // 2))
+    # else:
+    #     print(int(N // 2 + 1))
