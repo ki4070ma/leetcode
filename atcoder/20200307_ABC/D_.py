b5037415bbbaed32d0f205dbd3c559b069ed8f93
Atsushi Mori <ki4070ma@gmail.com>
2020-03-08 12:31:55 +0900

    [AtCoder] ABC 158 (D)

--- atcoder/20200307_ABC/D_.py
+++ atcoder/20200307_ABC/D_.py
@@ -4,6 +4,10 @@ for _ in range(3):
     S = input().rstrip()
     Q = int(input().rstrip())
     reversed = False
+    from collections import deque
+
+    q = deque()
+    q.extend(list(S))
     for _ in range(Q):
         l = input().rstrip()
         if l.startswith('1'):
@@ -12,18 +16,15 @@ for _ in range(3):
             _, F, C = l.split()
             if F == '1':
                 if reversed:
-                    S = S + C
+                    q.append(C)
                 else:
-                    S = C + S
-            else:
+                    q.appendleft(C)
+            elif F == '2':
                 if reversed:
-                    S = C + S
+                    q.appendleft(C)
                 else:
-                    S = S + C
-    if reversed:
-        ret = S[::-1]
-    else:
-        ret = S
+                    q.append(C)
+    ret = ''.join(list(q)[::-1]) if reversed else ''.join(list(q))
     print(ret)
     assert ret == input().rstrip()
     print(input().rstrip())
