8c06fcc9454ad9ca7b5a3910454d4d4674bb61e0
Atsushi Mori <ki4070ma@gmail.com>
2020-03-15 12:20:36 +0900

    [AtCoder] Panasonic (C)

---
+++ atcoder/20200314_Panasonic/C_.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+for _ in range(5):
+    a, b, c = map(int, input().rstrip().split())
+    from decimal import *
+
+    getcontext().prec = 500  # NG: 1000
+    if (Decimal(a).sqrt() + Decimal(b).sqrt()) < Decimal(c).sqrt():
+        print('Yes')
+    else:
+        print('No')
