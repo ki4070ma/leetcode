c20881b71244cf612f4c822901225938d1cf11c4
Atsushi Mori <ki4070ma@gmail.com>
2020-03-22 23:57:21 +0900

    [AtCoder] ABC 159 (B)

---
+++ atcoder/20200322_ABC/B_.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+for _ in range(3):
+    S = input().rstrip()
+    N = len(S)
+    if S == S[::-1] and \
+            S[:int((N - 1) / 2)] == S[:int((N - 1) / 2)][::-1] and \
+            S[int((N + 3) / 2) - 1:N] == S[int((N + 3) / 2) - 1:N][::-1]:
+        print('Yes')
+    else:
+        print('No')
