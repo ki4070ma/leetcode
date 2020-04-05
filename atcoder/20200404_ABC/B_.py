1a13b4d26a1849b3678f43ada423d1b98886f3ea
Atsushi Mori <ki4070ma@gmail.com>
2020-04-05 10:47:59 +0900

    [AtCoder] ABC 161 (B)

---
+++ atcoder/20200404_ABC/B_.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+for _ in range(3):
+    N, M = map(int, input().rstrip().split())
+    As = [int(x) for x in input().rstrip().split()]
+    # print(N, M)
+    # print(As)
+    total = sum(As)
+    count = len([True for x in As if x >= total / (4 * M)])
+    if count >= M:
+        print('Yes')
+    else:
+        print('No')
