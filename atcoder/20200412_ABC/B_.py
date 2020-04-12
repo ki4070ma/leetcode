67881f62d4e684ed08a7359a67502c4a0b94d770
Atsushi Mori <ki4070ma@gmail.com>
2020-04-12 22:46:16 +0900

    [AtCoder] ABC 162 B

---
+++ atcoder/20200412_ABC/B_.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+def chk_fizzbuzz(i):
+    if i % 3 == 0 or i % 5 == 0 or i % 15 == 0:
+        return 0
+    else:
+        return i
+
+for _ in range(2):
+    N = int(input().rstrip())
+    ret = 0
+    for i in range(N + 1):
+        ret += chk_fizzbuzz(i)
+    print(ret)
