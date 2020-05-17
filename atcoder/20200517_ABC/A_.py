054b3f79fc42d19b6c6aef70cc2f12b56aa805f5
Atsushi Mori <ki4070ma@gmail.com>
2020-05-17 23:34:08 +0900

    [AtCoder] ABC 168 A

---
+++ atcoder/20200517_ABC/A_.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+for _ in range(1):
+    S = input().rstrip()
+    S = int(list(S)[-1])
+    if S in [2, 4, 5, 7, 9]:
+        print('hon')
+    elif S in [0, 1, 6, 8]:
+        print('pon')
+    elif S in [3]:
+        print('bon')
+
