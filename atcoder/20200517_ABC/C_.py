230dc5e597cd66cae7284026061f985070090da2
Atsushi Mori <ki4070ma@gmail.com>
2020-05-17 23:35:31 +0900

    [AtCoder] ABC 168 C

---
+++ atcoder/20200517_ABC/C_.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+
+
+for _ in range(9):
+    import math
+
+    a, b, H, M = map(int, input().rstrip().split())
+    if H == 0 and M == 0:
+        print(abs(a - b))
+    else:
+        H = H * 5 + (float(M) / 12)
+        kakudo = abs(H - M)
+        kakudo *= 6
+        print(math.sqrt(float(a * a) + float(b * b) - 2 * a * b * math.cos(math.radians(kakudo))))
