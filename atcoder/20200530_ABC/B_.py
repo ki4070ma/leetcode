450e9cb8ae33f8b1411d138b45ecb91becfa0ad5
Atsushi Mori <ki4070ma@gmail.com>
2020-06-07 23:44:25 +0900

    [AtCoder] Nomura (B)

---
+++ atcoder/20200530_ABC/B_.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+
+
+for _ in range(1):
+    from collections import deque
+
+    T = input().rstrip()
+    d = deque()
+    d.extend(list(T))
+    answer = deque()
+    window = deque(maxlen=3)
+    for a in d:
+        if a == '?':
+            answer.append('D')
+        else:
+            answer.append(a)
+    print(''.join(answer))
+
