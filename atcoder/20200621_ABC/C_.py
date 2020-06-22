a19b0c6f22312dac5f09f6c5a9c330fdda853837
Atsushi Mori <ki4070ma@gmail.com>
2020-06-23 00:30:06 +0900

    [AtCoder] ABC 171 (B)

---
+++ atcoder/20200621_ABC/C_.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+for _ in range(7):
+    # ord('a'), chr(97)
+    N = int(input().rstrip())
+    ret = []
+    while N != 0:
+        char = chr(96 + (N % 26))
+        if char == '`':
+            char = 'z'
+            N = N // 26 - 1
+        else:
+            N = N // 26
+        ret.append(char)
+    print(''.join(ret[::-1]))
