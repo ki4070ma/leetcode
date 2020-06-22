e25eb1fc7ed0f052e8852d7d0ba7cad3f9bea5a0
Atsushi Mori <ki4070ma@gmail.com>
2020-06-22 22:59:53 +0900

    [AtCoder] ABC 170 (C)

---
+++ atcoder/20200613_ABC/C_.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+
+def show(ret):
+    print(' '.join([str(x) for x in ret]))
+
+for _ in range(3):
+    N, K = map(int, input().rstrip().split())
+    As = [int(x) for x in input().rstrip().split()]
+    show(As)
+    ret = []
+    for x in range(K):
+        ret = []
+        for y in range(len(As)):
+            add = 0
+            for z in range(len(As)):
+                if abs(y - z) <= As[z]:
+                    add += 1
+            ret.append(add)
+        As = ret[:]
+        show(ret)
+    show(ret)
+    print('----')
